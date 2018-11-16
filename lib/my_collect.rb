ea822c18a4b5437d3c31e2f81b70c177879fe7d6
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-16 23:05:32 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+empty_array = []
 
+def my_collect(empty_array)
+  collection = []
+  i = 0
+  while empty_array.length > i
+    collection << yield(empty_array[i])
+      i += 1
+  end
+   collection
+end
 
+my_collect(empty_array) {|name| name.upcase   }
