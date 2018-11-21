98a07160ce69cd83d168a8af9a56b5ad88165fa4
karen ercoli <karenercoli@gmail.com>
2018-11-21 22:46:00 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(collection)
+  collection = []
+  i = 0
+  while i < array.length
+    collection<<yield(array[i])
+    i += 1
+  end
+  collection
+end
 
 
+my_collect(collection) do |name|
+  name.split(" ").first
+end
