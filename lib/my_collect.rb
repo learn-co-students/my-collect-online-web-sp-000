57caea147b65d4fc315e699e06ddd185dad83dbe
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-12-18 00:41:25 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+  new_array = []
+  i = 0 
+  while i < array.length 
+    new_array << yield(array[i])
+    i += 1
+  end 
+  new_array
+end
 
-
+# def my_collect(array)
+#   i = 0
+#   collect = []
+#   while i < array.length
+#     collect << yield(array[i])
+#     i+=1
+#   end
+#   collect
+# end
