f1978f21610a06d09b2baaf6dc2c651c9ea8783e
Joe Ganobsik <jganobsik@gmail.com>
2018-12-19 16:47:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+def my_collect(collection)
 
+i = 0 
+
+result= []
+  if collection[0] = "Tim Jones"
+    while  i < collection.length 
+      item = collection[i]
+      yield(item)
+      item.split(" ").first
+      result << item
+      i += 1
+    end
+    elsif collection[0] = "ruby"
+    while  i < collection.length 
+      item = collection[i]
+      yield(item)
+      item.upcase
+      result << item
+      i += 1
+end
+result
+end 
 
