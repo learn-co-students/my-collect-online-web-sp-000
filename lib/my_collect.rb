d70a8d35a0d08fc3f9d243918e44e0146c89c601
Yingqi Chen <chanwk18@hotmail.com>
2019-03-25 02:52:22 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+  if block_given?
+    i=0
+    array2=[]
+    while i<array.length
+      array2 << yield(array[i])
+      i=i+1
+    end
+  else puts " NO BLOCK!!"
+  end
+  array2
+end
