9431b69f5eff907a2b66d87e6f74828998be3c58
Ashley Miller <asm2fe@virginia.edu>
2019-03-28 18:25:05 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
-
-
+def my_collect(array)
+  collection = []
+  i = 0
+  if array.empty?
+    return array
+  elsif array[0].include?(" ")
+    while i < array.length
+      collection << array[i].split(" ").first
+      i+=1
+    end
+    collection
+  else
+    while i < array.length
+      collection << array[i].upcase
+      i+=1
+    end
+    collection
+  end
+end
