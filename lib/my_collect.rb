4fdc05ac1fdb5236c91daa1bd1af764a2b593251
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-18 14:28:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(array)
+  array_pos = 0
+  collected_array = []
+  while array_pos < array.size
+    collected_array << yield(array[array_pos])
+    array_pos += 1
+  end
+  collected_array
+end
 
