5104c5788b5a7309b5ceeb39ffb66c81ad5e0372
elijahmm17 <elijahmm17@gmail.com>
2019-05-15 19:17:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+require 'pry'
 
+def my_collect(collection)
+  i = 0 
+  new_collection = []
+  while i < collection.size do
+    new_collection << yield(collection[i])
+    i += 1 
+  end
+  new_collection
+end 
 
+collection = ["eli", "aubrey", "lando", "michael"]
+
+my_collect(collection) do 
+  |name| name.upcase
+end 
