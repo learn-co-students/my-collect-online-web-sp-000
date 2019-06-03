1b8ca8ecdffc7eddd4baa251d2117876d5ffefe6
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-03 23:39:39 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if block_given?
+    count = 0
+    new_collection = []
+    while count < array.length
+      new_collection << yield(array[count])
+      count += 1
+    end
+    new_collection
+  else
+    return "This collection is empty."
+end
+end
