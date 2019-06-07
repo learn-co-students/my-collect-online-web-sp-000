ca012c48d564244fc8d0f06f4d5eda7909810b6b
Connor Cowan <ccowan003@comcast.net>
2019-06-07 23:40:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect(collection)
   i = 0                         #initialize counter
   new_collection = []           #declare new array for later
-  while i < collection.length   #condition: while there is a filled array
+  while i < collection.length   #condition: while there is a filled array (technically our iterator)
     new_collection << yield(collection[i]) #[] method to grab each successive index value as we go thru loop. Then push the return value of yielded array into our new_collection array
     i += 1                                #update counter
   end
