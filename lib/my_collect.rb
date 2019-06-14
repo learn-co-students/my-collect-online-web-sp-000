c44c0ccb1fbf2282b8f09e7986cfd944e48a859a
Frederic Silber <freddy.silber@cloudcoach.com>
2019-06-14 22:24:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+=begin
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    first = array[i].split(" ")[0]
+    first_upcase = first.upcase
+    collection << yield(first_upcase)
+    i += 1
+  end
+  collection
+end
+=end
 
-
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
