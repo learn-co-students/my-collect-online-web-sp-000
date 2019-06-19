be9e8e86d9eafb9a4163f48a9847320f150a2cc4
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-19 18:48:19 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,9 @@
-my_collect(array) do |name|
-  name.split(" ").first
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
 end
