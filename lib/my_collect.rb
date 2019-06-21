96d9c1dd30b6b415fbe2d88c3acb1e1412265e3d
Shawn <shawntennity@gmail.com>
2019-06-21 10:51:18 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(collection)
+  revised_collection = []
+  count = 0
 
+  while count < collection.length
+    revised_collection << yield(collection[count])
+    count += 1
+  end
+  revised_collection
+end
 
