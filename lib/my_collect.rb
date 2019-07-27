dcca37a2733c9b32a86eea058842e7fd7eba042d
Christopher Dent <christopherdent01@gmail.com>
2019-07-27 17:05:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
-
+def my_collect(array)
+    i = 0 
+    collection = []
+    while i < array.length 
+      collection << yield(array[i])
+      i = i + 1
+    end
+  p collection
+end 
+  
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+my_collect(students) do |name|
+  name.split(" ").first
+end 
+  
