b2a4595064ae45ae3c59cd398eea82d8db467891
Becky Zorn <bczorn@gmail.com>
2019-08-29 04:24:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) do |lang|
+  lang.upcase
+end
 
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |student|
+  student.split(" ").first
+end
