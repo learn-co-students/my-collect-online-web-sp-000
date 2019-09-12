05eaf950bfd11291b9547ced0f31369306684c71
Haim D <haim11293@aol.com>
2019-09-12 08:12:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,5 @@
+# define the my_collect method to take in an array, create an empty array to return at the end with the new data, set a counter to 0, use a while loop to loop through the array, yield to each element in the array nd add it to the new array, increment the counter by 1, return the new array with the modified data.
+
 def my_collect(array)
   new_array = []
   i = 0
