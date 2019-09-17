99741213d0c7d17f0a0fe953e43f16de03918ab2
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-17 06:21:04 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+  i = 0 
+  collection = []
+  while i < array.length 
+    collection.push(yield array[i])
+    i += 1 
+  end
+  return collection
+end
 
 
+
+# my_collect(languages) do |name, array|
+#     array.push(name.upcase)
+# end
+
+# my_collect(students) do |name, array|
+#     array.push(name.split(" ").first)
+# end)
