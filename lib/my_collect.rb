b7ee5bb08e0d483f52bc9ac48522b0bb1b97a148
Jack OMalley <jfo.omalley@gmail.com>
2019-10-06 16:07:41 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+require 'pry'
 
+def my_collect(array)
+  i = 0 
+  new_array = []
+  while i < array.length
+    yield(array[i])
+      if array[i].include?(" ")
+        new_array << array[i].split(" ").first
+      else
+        new_array << array[i].upcase
+      end
+    i += 1
+  end
+  new_array
+end
 
