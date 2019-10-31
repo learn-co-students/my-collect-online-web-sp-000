2c4ca3f7b537669e2a86ab0bc23f7c4980dc7906
Aoune Syed <aoune007@gmail.com>
2019-10-31 04:36:19 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+# example array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(array)
+  i = 0
+  collect_names = []
+  while i < array.length
+    collect_names << yield(array[i])
+    i+=1 # or i = i + 1
+  end
+  collect_names
+end
