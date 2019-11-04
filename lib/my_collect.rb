eb65d69d1fc7f9b065f827356c413a2b0626a244
Paul Marshall <pmarshal33@gmail.com>
2019-11-04 21:28:46 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+require 'pry'
 
+#write a method that behaves like the #collect method.
+def my_collect(arr)
+      i = 0 
+      collect = []
+      while i < arr.length
+#yield displays iteration/passes args and blocks of code
+        collect <<  yield(arr[i])
+         i += 1
+      end 
+  collect
+end
 
