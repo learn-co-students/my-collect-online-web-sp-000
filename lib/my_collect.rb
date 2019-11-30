eefc2d1934253a5e720fe561c3980ed0cfc4eab7
James Lenahan <james.k.lenahan@gmail.com>
2019-11-30 15:47:13 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
-
+def my_collect(array)
+  i = 0 
+  name_collection = []
+  while i < array.length 
+    name_collection.push yield(array[i])
+    i += 1 
+  end
+  return name_collection
+end
+  
