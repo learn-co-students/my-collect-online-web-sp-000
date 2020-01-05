f6ac1ef832f19fb257f574ec6ac56258ac76203a
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-05 00:31:00 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(collection)
   i = 0 
   new_collection = []
   while i < collection.length
-    new_collection << yield(collection[i])
+    new_collection.push(yield(collection[i]))
     i += 1 
   end 
   new_collection
