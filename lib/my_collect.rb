87b96a782037c17bd2ffff6dd3e548d77665515d
Mark A Guza <guzamaguza@gmail.com>
2020-01-16 05:47:56 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+require 'pry'
 
-
+def my_collect(array)
+  i = 0
+  ary = []
+  while i < array.length
+    yield(array[i]) if block_given?
+    ary << yield(array[i])
+    #name.split(" ").first
+    #lang.upcase
+    i +=1
+  end
+  return ary
+end
