fa85b64ad2bab04d0b0925acc14049e88a1b2342
Alvin Coulson <aaacoulson@gmail.com>
2020-02-20 01:33:29 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  i = 0
+  
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  return collection
+  array
+end
 
-
+my_collect( ['ruby', 'javascript', 'python', 'objective-c']) {|lang| lang.upcase}
+my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']) {|name| name.split(" ").first}
