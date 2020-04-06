e8c897aaedbaa56c47f16c553656dc5fbcf6a6b8
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-06 13:26:43 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+require 'pry'
 
+def my_collect(x)
+  i = 0
+  array = []
+  # binding.pry
+  while i < x.length
+    array << yield(x[i])
+    i += 1
+  end
+  array
+end
 
+# my_collect([languages]) {|lang| lang.upcase}
+# my_collect([students]) {|name| name.split(" ").first}
