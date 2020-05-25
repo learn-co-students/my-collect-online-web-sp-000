5d460184e774e9bc3455018cc8228b7e09a47a22
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-25 18:40:21 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(arg)
+  i = 0
+  output = []
+  while i < arg.length
+    output << yield(arg[i])
+    i += 1
+  end
+  output
+end
+# 
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end
