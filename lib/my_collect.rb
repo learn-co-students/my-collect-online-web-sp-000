3f5aa048f07bb4c4d1f5e00923427b2192642ad2
Dean Erickson <dmepilot@yahoo.com>
2020-06-08 16:00:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  if block_given?
+    new_collection = []
+    i = 0 
+    while i < array.length 
+      new_collection << yield(array[i])
+      i += 1 
+  end
+  new_collection
+end
+end
 
-
+  
+  
