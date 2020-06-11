b6dd555bb8a0b0b7d47caebf5205712ab2a52757
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-11 16:34:17 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect (collection)
+  i = 0
+  final_collection = []
+  if collection.length == 0
+      return 10
+  else
+    while i < collection.length
+      yield collection[i]
+      final_collection << yield(collection[i])
+      i += 1
+    end
+  end
+  return final_collection
+end
