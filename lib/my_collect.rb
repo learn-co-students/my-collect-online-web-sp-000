65d1408740b5db86f7fd984c53caa27762e8b363
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-19 22:33:17 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    results = []
+    while i < array.length
+      results << yield(array[i])
+      i += 1
+    end
+    results
+  else
+    "There is no block here!"
+end
+end
