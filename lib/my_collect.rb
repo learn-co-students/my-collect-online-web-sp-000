75b73757cd2205fbf064d12d3acb113a6b66e2b6
Rosario Holsen <rholsen@gmail.com>
2020-08-19 01:16:50 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(collection)
+  if block_given?
+    i = 0 
+    array = []
+    while i < collection.length 
+      array << yield(collection[i])
+      i += 1 
+    end
+    array
+  else
+    false 
+  end 
+end 
 
