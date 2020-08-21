73d1e16bcd122f93e854b7a910862967c47ca435
John Stassen <john.stassen@gmail.com>
2020-08-21 23:21:40 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+  counter = 0
+  new_array = []
+  while counter < array.length
+    new = yield array[counter]
+    new_array << new
+    counter += 1
+  end
+  new_array
+end
