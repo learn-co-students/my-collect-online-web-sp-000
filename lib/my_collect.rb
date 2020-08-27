fa4b53faa2ba42f49c1e8c2a8ebeee139d52c8f2
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-27 19:14:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(names)
   x = 0 
   dif_collections = []
   while names.length > x 
-    dif_collections << yield (names[x])
+    dif_collections << yield(names[x])
     x += 1 
   end
   dif_collections
