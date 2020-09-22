1326e48f801a2fbb670137659557a2afb0542030
Chandra Lama <iamme24cl@gmail.com>
2020-09-22 14:21:00 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+    i = 0
+    capitalized_array = []
+    while i < array.length
+        capitalized_array << yield(array[i])
+        i += 1
+    end
+    capitalized_array
+end
+
 
 
