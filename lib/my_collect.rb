64a563d9680cb7032394021a18eaf518c7a7a071
Ryan K Burns <ryankburns@gmail.com>
2020-09-23 17:03:54 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(languages)
+  i = 0
+  new_lang = []
+  while i < languages.length
+    new_lang << yield(languages[i].upcase)
+      i += 1
+    end
+    new_lang
+end
 
-
+def my_collect(names)
+  i = 0
+  new_names = []
+  while i < names.length
+    new_names << yield(names[i].split(" ").first)
+      i += 1
+    end
+    new_names
+end
