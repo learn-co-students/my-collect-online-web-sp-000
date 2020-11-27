b59a88d5e5e232bbe8a0f4e6a8a375c81f8d6d58
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-27 19:55:52 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(language)
+  if language = []
+  end 
+   else 
+     while counter <= language.length 
+  value = language.upcase 
+  end 
+  value 
+end 
+ 
+    
+
+def my_collect(students)
 
 
