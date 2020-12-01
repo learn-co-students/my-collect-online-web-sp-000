a795949bacda4f36e0c0006ab0bce84138752416
Hannah Malm <hannahemalm616@gmail.com>
2020-12-01 00:11:22 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(array)
+  i = 0
+  #my_collect can handle an empty collection 
+  collection = [] 
+  while i < array.length
+  #my_collect returns a new collection of appropriately motidifed elements
+    collection << 
+    #yields the correct element from the collection
+    yield(array[i])
+    i += 1
+  end
+  #returns a collection
+  collection 
+end
+
 
 
