a5172110fb8495bf4eaf189ad7894910dc2e09a5
Julia Kim <juliaykim6@gmail.com>
2021-01-18 02:49:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect(array)
+  index = 0
+  new_collection = []
+  while index < array.length
+    new_collection << yield(array[index])
+    index += 1
+end
+new_collection
+end
 
 
+#my_collect(array) do |name|
+#  name.split(" ").first
+#end
+
+
+
+
+
+
+#Build a method that yields members of a collection to a block.
+#Control the return value of a method that uses yield
+# such that it returns a new collection.
