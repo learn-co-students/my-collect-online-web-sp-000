6e72e3686a8be449a2a7fac6668b5bee34276e3e
Alexander Hulteng <alexanderhulteng@gmail.com>
2021-02-12 01:25:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(collection)
+  if block_given?
+    i = 0
+    names = []
+    while i < collection.length
+      names << yield(collection[i])
+      i += 1
+    end
+  end
+  names
 
-
+end
