308bcb6b8d666a7acb3c9e5bd16175e2aa6b8f07
Becky Rosa <becky_m_rosa@yahoo.com>
2021-02-22 02:21:04 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+  a = 0
+  collection = []
+   array.each do |e|
+    collection <<  yield(e)
+  a += 1
+end
+collection
+end
 
-
+my_collect(["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]) { |word| "#{word}" }
