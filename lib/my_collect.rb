01eeb9df049eae1ddeafb8727f499210a7d417dc
James Tillman <james@itme-music.com>
2021-05-31 16:17:27 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+#rebuilding collect, ignoring the collect method
+def my_collect(collection)
+  i = 0
+  for_later = []
+  while i < collection.size
+    for_later << yield(collection[i])
+    i+= 1
+  end
+  for_later
+end
